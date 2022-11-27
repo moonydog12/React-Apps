f4207a410a69b561069f69390071a61590dedf7a
xolodog <losididog@gmail.com>
2022-11-27 14:18:21 +0800

    Use useContext and custom hook to refractor

--- Books/src/Components/BookCreate.jsx
+++ Books/src/Components/BookCreate.jsx
@@ -1,26 +1,29 @@
 import { useState } from 'react';
+import useBooksContext from '../hooks/use-books-context';
 
-const BookCreate = ({ onCreate }) => {
+const BookCreate = () => {
   const [title, setTitle] = useState('');
+  const { createBook } = useBooksContext();
+
   const handleChange = (event) => {
     setTitle(event.target.value);
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    onCreate(title);
+    createBook(title);
 
     // Clear the input field
     setTitle('');
   };
 
   return (
-    <div className='book-create'>
+    <div className="book-create">
       <h3>Add a Book</h3>
       <form onSubmit={handleSubmit}>
         <label>Title</label>
-        <input className='input' value={title} onChange={handleChange} />
-        <button className='button'>Create!</button>
+        <input className="input" value={title} onChange={handleChange} />
+        <button className="button">Create!</button>
       </form>
     </div>
   );
--- Books/src/main.jsx
+++ Books/src/main.jsx
@@ -1,10 +1,13 @@
-import './Components/index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { Provider } from './context/books';
+import './Components/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider>
+      <App />
+    </Provider>
   </React.StrictMode>,
 );
