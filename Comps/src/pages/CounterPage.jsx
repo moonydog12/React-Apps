18c7dc6ecc0398b4b590747a0825b3ba06e43006
lsd <lsd@ehk.com.tw>
2022-12-14 16:19:03 +0800

    Implement reducer function

--- Comps/src/pages/CounterPage.jsx
+++ Comps/src/pages/CounterPage.jsx
@@ -1,15 +1,90 @@
+import { useReducer } from 'react';
 import Button from '../components/Button';
-import useCounter from '../hooks/use-counter';
+import Panel from '../components/Panel';
+
+const INCREMENT_COUNT = 'increment';
+const DECREMENT_COUNT = 'decrement';
+const SET_VALUE_TO_ADD = 'change_value_to_add';
+const ADD_VALUE_TO_COUNT = 'add_value_to_count';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case INCREMENT_COUNT:
+      return {
+        ...state,
+        count: state.count + 1,
+      };
+    case DECREMENT_COUNT:
+      return {
+        ...state,
+        count: state.count - 1,
+      };
+    case SET_VALUE_TO_ADD:
+      return {
+        ...state,
+        valueToAdd: action.payload,
+      };
+    case ADD_VALUE_TO_COUNT:
+      return {
+        ...state,
+        count: state.count + state.valueToAdd,
+        valueToAdd: 0,
+      };
+    default:
+      return state;
+  }
+};
 
 function CounterPage({ initialCount }) {
-  const { count, increment } = useCounter(initialCount);
+  const [state, dispatch] = useReducer(reducer, {
+    count: initialCount,
+    valueToAdd: 0,
+  });
+
+  const increment = () => {
+    dispatch({ type: INCREMENT_COUNT });
+  };
+
+  const decrement = () => {
+    dispatch({ type: DECREMENT_COUNT });
+  };
+
+  const handleChange = (event) => {
+    const value = +event.target.value || 0;
+    dispatch({ type: SET_VALUE_TO_ADD, payload: value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    dispatch({ type: ADD_VALUE_TO_COUNT });
+  };
+
   return (
-    <>
-      <h1>Count is {count}</h1>
-      <Button danger outline onClick={increment}>
-        Increase
-      </Button>
-    </>
+    <Panel className="m-3">
+      <h1 className="text-lg">
+        Count is <span className="text-sky-500">{state.count}</span>
+      </h1>
+      <div className="flex flex-row">
+        <Button primary onClick={increment}>
+          Increase
+        </Button>
+
+        <Button danger onClick={decrement}>
+          Decrement
+        </Button>
+      </div>
+
+      <form onSubmit={handleSubmit}>
+        <label>Add a lot!</label>
+        <input
+          value={state.valueToAdd || ''}
+          onChange={handleChange}
+          type="number"
+          className="p-1 m-3 bg-gray-50 border border-gray-300"
+        />
+        <Button>Add it!</Button>
+      </form>
+    </Panel>
   );
 }
 
