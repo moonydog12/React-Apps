e2e6c61ce4493b34dc3a8a59f5fc17c81f0cdf19
xolodog <losididog@gmail.com>
2022-12-17 23:05:01 +0800

    Customize use-thunk hook

--- Media(async)/src/components/UserList.jsx
+++ Media(async)/src/components/UserList.jsx
@@ -1,24 +1,31 @@
 import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchUsers } from '../store';
+import { useSelector } from 'react-redux';
+import { fetchUsers, addUser } from '../store';
+import Button from './Button';
 import Skeleton from './Skeleton';
+import { useThunk } from '../hooks/use-thunk';
 
 function UserList() {
-  const dispatch = useDispatch();
-  const { isLoading, data, error } = useSelector((state) => {
+  const [doFetchUsers, isLoadingUsers, loadingUsersError] = useThunk(fetchUsers);
+  const [doCreateUser, isCreatingUser, creatingUserError] = useThunk(addUser);
+  const { data } = useSelector((state) => {
     // {data:[],isLoading:false,error:null}
     return state.users;
   });
 
   useEffect(() => {
-    dispatch(fetchUsers());
+    doFetchUsers();
   }, []);
 
-  if (isLoading) {
+  const handleUserAdd = () => {
+    doCreateUser();
+  };
+
+  if (isLoadingUsers) {
     return <Skeleton times={6} className="h-10 w-full"></Skeleton>;
   }
 
-  if (error) {
+  if (loadingUsersError) {
     return <div>Error fetching data...</div>;
   }
 
@@ -30,7 +37,16 @@ function UserList() {
     );
   });
 
-  return <div>{renderedUsers}</div>;
+  return (
+    <div>
+      <div className="flex flex-row justify-between m-3">
+        <h1 className="m-2 text-xl">Users</h1>
+        {isCreatingUser ? 'Creating User...' : <Button onClick={handleUserAdd}>+Add user</Button>}
+        {creatingUserError && 'error creating user...'}
+      </div>
+      {renderedUsers}
+    </div>
+  );
 }
 
 export default UserList;
